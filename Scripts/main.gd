fe916130dfcaf596c6c3038366d6f1695ded0b5d
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2025-06-17 12:49:25 +0200

    Made the timer countdown
    I created a Timer node and a script that displays the countdown with the TimerLabel

---
+++ Scripts/main.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+func timerLabelUpdate(time):
+	var minutes = str(int(time/60))
+	var seconds = str(int(fmod(time, 60)))
+	if minutes.length() == 1:
+		minutes = "0" + minutes
+	if seconds.length() == 1:
+		seconds = "0" + seconds
+	$MarginContainer/HBoxContainer/TimerPanel/TimerLabel.text = "%s:%s" % [minutes, seconds]
+	
+func _ready():
+	$Timer.start()
+	
+func _process(_delta):
+	var time = $Timer.time_left
+	timerLabelUpdate(time)
